0cf266641d6de5cb2144459ec45a9f7ef0db1ea4
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-03-30 16:20:46 +0200

    Issue 6 solved. Test for review model is passed.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ActiveRecord::Base
+  # Associations
   belongs_to :product
   belongs_to :user
+
+  # Validates uniqueness of name
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :user_id, presence: true
+
 end
