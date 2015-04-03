4b8a26e1648398227a39daba2c768bc619fa2103
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-04-03 11:55:18 +0200

    Add Product, Review data to seeds.rb. Correct spelling errors in previous data records in the same file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
 # Data for User
 
 user_01 = User.create(
@@ -83,16 +74,101 @@ cat_01 = Category.create(
 cat_01.save!
 
 cat_02 = Category.create(
-    name: 'hats' )
+            name: 'hats' )
 
 cat_02.save!
 
 cat_03 = Category.create(
-    name: 'umbrella' )
+            name: 'umbrella' )
 
 cat_03.save!
 
 cat_04 = Category.create(
-    name: 'bags' )
+            name: 'bags' )
 
 cat_04.save!
+
+# Data for Products
+# Category - shoes - should be 8 products here
+
+product_s_01 = Product.create(
+            title: 'lace up',
+            price: 150.00,
+            description: 'Comfortable shoes.',
+            category_id: 1,
+            user_id: 4 )
+
+product_s_01.save!
+
+# Category - hats - should be 8 products here
+
+product_h_01 = Product.create(
+            title: 'stetson',
+            price: 200.00,
+            description: 'Hat for courageous type of man',
+            category_id: 2,
+            user_id: 1 )
+
+product_h_01.save!
+
+# Category - umbrella - should be 8 products here
+
+product_u_01 = Product.create(
+            title: 'classic',
+            price: 20.00,
+            description: 'Classic style umbrella for daily use',
+            category_id: 3,
+            user_id: 3 )
+
+product_u_01.save!
+
+# Category - bags - should be 8 products here
+
+product_b_01 = Product.create(
+            title: 'handbag',
+            price: 50.00,
+            description: 'Bag for a city stroll',
+            category_id: 4,
+            user_id: 5 )
+
+product_b_01.save!
+
+# Review - shoes - should be 5 reviews here
+
+review_s_01 = Review.create(
+    content: 'Comfortable',
+    rating: 5,
+    product_id: 1,
+    user_id: 5 )
+
+review_s_01.save!
+
+# Review - hats - should be 5 reviews here
+
+review_h_01 = Review.create(
+    content: 'Stylish',
+    rating: 4,
+    product_id: 3,
+    user_id: 2 )
+
+review_h_01.save!
+
+# Review - umbrella - should be 5 reviews here
+
+review_h_01 = Review.create(
+    content: 'Good looking',
+    rating: 5,
+    product_id: 4,
+    user_id: 5 )
+
+review_h_01.save!
+
+# Review - bags - should be 5 reviews here
+
+review_h_01 = Review.create(
+    content: 'Spacious',
+    rating: 3,
+    product_id: 2,
+    user_id: 5 )
+
+review_h_01.save!
