4c4fde836d269cf66ab6071a4b0c235457edf1bc
michal zagrodzki <michalzagrodzki@gmail.com>
2015-04-07 21:50:54 +0200

    Solved one issue from point 4. Update readme.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,14 @@ class Product < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
 
-  # Validates presence of title, description, price
+  # Validates presence of title, description, price; Validates rounded value in price.
   validates :title, presence: true
   validates :description, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
+  # :greater_than_or_equal_to => 0.01 - check this for :price
+
+  # Defines average rating of reviews
+  def average_rating
+
+  end
 end
