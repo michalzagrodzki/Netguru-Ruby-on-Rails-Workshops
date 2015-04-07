98c2dc3944ae8865ecc2db2438e886817a572308
michal zagrodzki <michalzagrodzki@gmail.com>
2015-04-07 21:51:38 +0200

    Validate seeds.rb. Data now loads correctly into database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,67 +3,61 @@
 user_01 = User.create(
              firstname: 'John',
              lastname: 'Steinbeck',
-             email: "john@steinbeck",
+             email: "john@steinbeck.com",
              password: 'john1234',
              password_confirmation: 'john1234',
              admin: false )
 
-user_01.skip_confirmation!
 user_01.save!
 
 user_02 = User.create(
             firstname: 'Albert',
             lastname: 'Camus',
-            email: "albert@camus",
+            email: "albert@camus.int",
             password: 'albert1234',
             password_confirmation: 'albert1234',
             admin: false )
 
-user_02.skip_confirmation!
 user_02.save!
 
 user_03 = User.create(
             firstname: 'Josephine',
             lastname: 'Johnson',
-            email: "josephine@johnson",
+            email: "josephine@johnson.com",
             password: 'josephine1234',
             password_confirmation: 'josephine1234',
             admin: false )
 
-user_03.skip_confirmation!
 user_03.save!
 
 user_04 = User.create(
             firstname: 'William',
             lastname: 'Faulkner',
-            email: "william@faulkner",
-            password: 'wailliam1234',
-            password_confirmation: 'wailliam1234',
+            email: "william@faulkner.int",
+            password: 'william1234',
+            password_confirmation: 'william1234',
             admin: false )
 
-user_04.skip_confirmation!
 user_04.save!
 
 user_05 = User.create(
             firstname: 'Willa',
             lastname: 'Cather',
-            email: "willa@cather",
+            email: "willa@cather.int",
             password: 'willa1234',
             password_confirmation: 'willa1234',
             admin: false )
 
-user_05.skip_confirmation!
 user_05.save!
 
 user_06 = User.create(
             firstname: 'Ernest',
             lastname: 'Hemingway',
-            email: "ernest@hemingway",
+            email: "ernest@hemingway.int",
             password: 'ernest1234',
             password_confirmation: 'ernest1234',
             admin: true )
 
-user_06.skip_confirmation!
 user_06.save!
 
 # Data for Category
