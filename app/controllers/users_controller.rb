40635594b6c28981593b962d1c6c42cd1b338c52
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-04-14 16:10:51 +0200

    Add permission to use User, Product and Review models in User View.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
+  expose(:user)
+  expose(:review)
+  expose(:product)
+
   def index
     # description of @users is not necessary - it is handled by decent_exposure
   end
