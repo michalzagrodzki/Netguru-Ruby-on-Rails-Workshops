b4e57cb1660038a410eea6c6adbfcdfacf13a102
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-04-14 15:20:07 +0200

    Create routes for user profile and user index. Add comments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   resources :categories do
     resources :products do
       resources :reviews
@@ -7,8 +8,13 @@ Rails.application.routes.draw do
 
   get 'categories/index'
 
+  # route for setting welcome page
   root 'categories#index'
 
   # routes for users are carried out by Devise
   devise_for :users
+
+  # routes for showing user profile, show - for current user, index - for admin.
+  resources :users, only: [:show, :index]
+
 end
