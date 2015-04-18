b14c4b827cce5715e0ddca37abb1642ee19d2c4a
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-04-18 19:09:43 +0200

    Add helper for adding images with links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  # Link wrapped in image
+  def link_in_image(image_path, target_link, options={})
+    link_to(image_tag(image_path, options), target_link)
+  end
+
 end
