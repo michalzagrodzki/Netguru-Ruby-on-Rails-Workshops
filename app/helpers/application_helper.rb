53f603eb06db47b966ea18b865188db2daf21244
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-04-18 19:59:13 +0200

    Correct link_in_image helper, now includes options for link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
   # Link wrapped in image
-  def link_in_image(image_path, target_link, options={})
-    link_to(image_tag(image_path, options), target_link)
+  def link_in_image(image_path, target_link, options_image={}, options_link={})
+    link_to(image_tag(image_path, options_image), target_link, options_link)
   end
 
 end
